d6554908e1d5c430486088ddb9421fba456cbaf1
Adhitama Chandra <chandra.y.adhitama@gmail.com>
2024-12-08 13:43:47 +0700

    add meta data and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Your First Business Partner in The Export of Premium Spices',
+      meta: [
+        { 
+          name: 'description', 
+          content: 'Your trusted partner in exporting premium spices. We deliver high-quality export services to meet the demands of the global market.' 
+        },
+        { 
+          name: 'viewport', 
+          content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' 
+        },
+      ],
+      link: [
+        { rel: 'icon', type: '/image/x-icon', href: '/logo.webp' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+    },
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/css/tailwind.css', 'swiper/swiper-bundle.css'],
@@ -34,9 +53,5 @@ export default defineNuxtConfig({
     display: 'swap',
     prefetch: true,
     preconnect: true,
-  },
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000,
   },
 })
